30397bf75758982ae9ce5f4bf1dc6d3346f8faff
october03 <season_autumn@kakao.com>
2024-02-09 17:32:19 +0900

    initial commit

---
+++ src/main/kotlin/com/october03/cafe/kiosk/coupon/KioskCouponServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.october03.cafe.kiosk.coupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KioskCouponServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<KioskCouponServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/october03/cafe/kiosk/coupon/KioskCouponServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.october03.cafe.kiosk.coupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KioskCouponServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
