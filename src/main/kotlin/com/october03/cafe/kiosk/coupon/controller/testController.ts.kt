63a14dd3ca7d16458c8322f3044d4668d30c49fa
october03 <season_autumn@kakao.com>
2024-02-10 10:56:16 +0900

    테스트 컨트롤러 생성

---
+++ src/main/kotlin/com/october03/cafe/kiosk/coupon/controller/testController.ts.kt
@@ -0,0 +1,13 @@
+package com.october03.cafe.kiosk.coupon.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/coupon")
+class TestController {
+  @RequestMapping("/test")
+  fun test(): String {
+    return "Hello World"
+  }
+}
